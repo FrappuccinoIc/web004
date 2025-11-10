55226b9123cd124b8f7c564ab5f6aec2f10afe13
frappuccinoic <arseeicore@gmail.com>
2025-11-10 10:13:27 -0300

    Registro de modelos en panel admin

--- ordenes/admin.py
+++ ordenes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client, Category, Ot
 
-# Register your models here.
+#class CategoryAdmin(admin.ModelAdmin)
+
+admin.site.register(Client)
+admin.site.register(Category)
+admin.site.register(Ot)
